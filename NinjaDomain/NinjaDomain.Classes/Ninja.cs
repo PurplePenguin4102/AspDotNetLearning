91b1cf59a98c8e926c0e9faa3443a17dbdb9cc99
JoeyRay <joseph.h.ray@gmail.com>
2017-02-24 16:33:35 +1000

    Added modification logic to DataContext

--- NinjaDomain/NinjaDomain.Classes/Ninja.cs
+++ NinjaDomain/NinjaDomain.Classes/Ninja.cs
@@ -1,12 +1,11 @@
-﻿using System;
+﻿using NinjaDomain.Classes.Interfaces;
+using System;
 using System.Collections.Generic;
-using NinjaDomain.Classes.Enums;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace NinjaDomain.Classes
 {
-    public class Ninja
+    public class Ninja : IModificationHistory
     {
         public Ninja()
         {
@@ -21,25 +20,9 @@ namespace NinjaDomain.Classes
         public virtual List<NinjaEquipment> EquipmentOwned { get; set; }
         [Column(TypeName = "DateTime2")]
         public DateTime DateOfBirth { get; set; }
-    }
 
-    public class Clan
-    {
-        public Clan()
-        {
-            Ninjas = new List<Ninja>();
-        }
-        public int Id { get; set; }
-        public string ClanName { get; set; }
-        public List<Ninja> Ninjas { get; set; }
-    }
-
-    public class NinjaEquipment
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public EquipmentType Type { get; set; }
-        [Required]
-        public Ninja Ninja { get; set; }
+        public DateTime DateModified { get; set; }
+        public DateTime DateCreated { get; set; }
+        public bool IsDirty { get; set; }
     }
 }
--- NinjaDomain/NinjaDomain.DataModel/NinjaContext.cs
+++ NinjaDomain/NinjaDomain.DataModel/NinjaContext.cs
@@ -1,5 +1,8 @@
 ﻿using System.Data.Entity;
 using NinjaDomain.Classes;
+using NinjaDomain.Classes.Interfaces;
+using System;
+using System.Linq;
 
 namespace NinjaDomain.DataModel
 {
@@ -8,5 +11,37 @@ namespace NinjaDomain.DataModel
         public DbSet<Ninja> Ninjas { get; set; }
         public DbSet<Clan> Clans { get; set; }
         public DbSet<NinjaEquipment> Equipment { get; set; }
+
+        // we can use this method to tap into the model creation process and
+        // hook in our own configuration
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // we tell the model to ignore a field
+            modelBuilder.Types().Configure(c => c.Ignore("IsDirty"));
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override int SaveChanges()
+        {
+            foreach (var history in this.ChangeTracker.Entries()
+                .Where(entity => entity.Entity is IModificationHistory && (entity.State == EntityState.Added || entity.State == EntityState.Modified))
+                .Select(e => e.Entity as IModificationHistory))
+            {
+                history.DateModified = DateTime.Now;
+                if (history.DateCreated == DateTime.MinValue)
+                {
+                    history.DateCreated = DateTime.Now;
+                }
+                
+            }
+            int result =  base.SaveChanges();
+            foreach (var history in this.ChangeTracker.Entries()
+                .Where(e => e.Entity is IModificationHistory)
+                .Select(e => e.Entity as IModificationHistory))
+            {
+                history.IsDirty = false;
+            }
+            return result;
+        }
     }
 }
